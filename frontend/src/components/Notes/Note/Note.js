9f5f694ae5138497e78c048f639cfc860908cfae
PiotrWR <pwrynio@gmail.com>
2021-06-25 18:56:23 +0200

    Add description showing function.

--- frontend/src/components/Notes/Note/Note.js
+++ frontend/src/components/Notes/Note/Note.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function Note({ body, title, onDelete, id }) {
+  const [isShowedDescription, setIsShowedDescription] = useState(false);
+
+  const toggleDescription = () => setIsShowedDescription(!isShowedDescription);
+
   return (
     <div className="note">
-      <p>{title}</p>
-      <div className="description">{body}</div>
+      <p onClick={toggleDescription}>{title}</p>
+      {isShowedDescription && <div className="description">{body}</div>}
       <button>edytuj</button>
       <button
         className="delete"
