92d1b7145dc10aef6e1199c7070b9195771b69e4
PiotrWR <pwrynio@gmail.com>
2021-06-25 22:57:17 +0200

    Add showing new note form

--- frontend/src/components/Notes/NewNote/NewNote.jsx
+++ frontend/src/components/Notes/NewNote/NewNote.jsx
@@ -2,28 +2,34 @@ import React, {useState} from 'react';
 
 function NewNote(props) {
 
+  const [isShowingForm, setIsShowingForm] = useState(false);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
   const onChangeDescriptionHandler = value => setDescription(value);
 
   const onAddNote = () => {
-    const note = {title, body:description}
+    const note = {title, body: description}
     props.onAddNote(note)
     setTitle('');
     setDescription('')
+    setIsShowingForm(false)
   }
 
   return (
-    <div className="note">
-      <label>Tytuł</label>
-      <input type="text" value={title} onChange={event => setTitle(event.target.value)}/>
+    isShowingForm ?
+     ( <div className="note">
+        <label>Tytuł</label>
+        <input type="text" value={title} onChange={event => setTitle(event.target.value)}/>
 
-      <label>Opis:</label>
-      <input type="text" value={description} onChange={event => onChangeDescriptionHandler(event.target.value)}/>
+        <label>Opis:</label>
+        <input type="text" value={description} onChange={event => onChangeDescriptionHandler(event.target.value)}/>
 
-      <button onClick={onAddNote}>Dodaj notatkę!</button>
-    </div>
+        <button onClick={onAddNote}>Dodaj notatkę!</button>
+      </div>)
+      :
+      <button onClick={()=>setIsShowingForm(true)}>Nowa notatka</button>
+
   );
 }
 
