ccb9da90ab470f9a0469d5db7295446083cc9d29
PiotrWR <pwrynio@gmail.com>
2021-06-26 13:16:37 +0200

    Add cors and cors library

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,13 @@ const { port } = require('../config');
 const app = express();
 const apiRouter = require('./routes/api');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 require('./db/mongoose');
 
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.use('/api/', apiRouter);
 
