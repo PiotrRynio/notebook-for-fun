ff1bbccb7d673d1d6e4918d7db8719ae91c952d1
PiotrWR <pwrynio@gmail.com>
2021-06-27 02:12:20 +0200

    Add .env in Frontend and axios instance

--- frontend/src/common/constants/apiPaths.js
+++ frontend/src/common/constants/apiPaths.js
@@ -1,2 +1,2 @@
-export const PATH_BASE_URL = 'http://localhost:5000/api';
-export const PATH_NOTES_URL = PATH_BASE_URL + '/notes';
+export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+export const NOTES_URL_PATH = '/notes';
--- frontend/src/components/Notes/Notes.js
+++ frontend/src/components/Notes/Notes.js
@@ -4,8 +4,8 @@ import { Note } from './Note/Note';
 import NewNote from './NewNote/NewNote';
 import Modal from 'react-modal';
 import { EditNote } from './EditNote/EditNote';
-import axios from 'axios';
-import { PATH_NOTES_URL } from '../../common/constants/apiPaths';
+import axios from '../../common/axios';
+import { NOTES_URL_PATH } from '../../common/constants/apiPaths';
 
 export function Notes() {
   const startingNotesBD = [];
@@ -18,18 +18,18 @@ export function Notes() {
     fetchNotes().then();
   }, [editNote, isEditFormOpened]);
 
-  const fetchNotes = async () => await axios.get(PATH_NOTES_URL).then(({ data }) => setNotes(data));
+  const fetchNotes = async () => await axios.get(NOTES_URL_PATH).then(({ data }) => setNotes(data));
 
   const addNote = async (note) =>
-    await axios.post(PATH_NOTES_URL, note).then(({ data }) => setNotes([...notes].concat(data)));
+    await axios.post(NOTES_URL_PATH, note).then(({ data }) => setNotes([...notes].concat(data)));
 
   const deleteNote = async (_id) => {
-    await axios.delete(PATH_NOTES_URL + '/' + _id);
+    await axios.delete(NOTES_URL_PATH + '/' + _id);
     setNotes([...notes].filter((note) => _id !== note._id));
   };
 
   const onEditNote = async (note) => {
-    await axios.put(PATH_NOTES_URL + '/' + note._id, { note });
+    await axios.put(NOTES_URL_PATH + '/' + note._id, { note });
     const returnedNotes = [...notes];
     const editNoteIndex = returnedNotes.findIndex(({ _id }) => _id === note._id);
     returnedNotes[editNoteIndex] = note;
